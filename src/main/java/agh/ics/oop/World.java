b4ae899b8cbd748a2a4bd226143da47fcb7a08e0
MCzech3605 <mczech@student.agh.edu.pl>
2022-10-13 13:33:27 +0200

    Lab1

---
+++ src/main/java/agh/ics/oop/World.java
@@ -0,0 +1,41 @@
+package agh.ics.oop;
+
+import static java.lang.System.out;
+
+public class World {
+    public static void run(Direction[] args)
+    {
+        int n = args.length;
+        for(Direction arg : args)
+        {
+            String output = switch (arg)
+            {
+                case FORWARD -> "Zwierzak idzie do przodu\n";
+                case BACKWARD -> "Zwierzak idzie do tyłu\n";
+                case LEFT -> "Zwierzak skręca w lewo\n";
+                case RIGHT -> "Zwierzak skręca w prawo\n";
+                default -> "";
+            };
+            out.print(output);
+        }
+    }
+    public static void main(String[] args) {
+        out.println("Start");
+        int n = args.length;
+        Direction[] toFunction;
+        toFunction = new Direction[n];
+        for(int i=0; i<n; i++)
+        {
+            Direction temp = switch (args[i]) {
+                case "f" -> Direction.FORWARD;
+                case "b" -> Direction.BACKWARD;
+                case "l" -> Direction.LEFT;
+                case "r" -> Direction.RIGHT;
+                default -> Direction.UNKNOWN;
+            };
+            toFunction[i] = temp;
+        }
+        run(toFunction);
+        out.print("Stop");
+    }
+}
