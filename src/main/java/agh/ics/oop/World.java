80115779ad1c0cf452f17a4a5531fc4c88521ae9
MCzech3605 <mczech@student.agh.edu.pl>
2022-11-02 18:24:02 +0100

    Lab4

--- src/main/java/agh/ics/oop/World.java
+++ src/main/java/agh/ics/oop/World.java
@@ -36,8 +36,8 @@ public class World {
 
     public static void main(String[] args) {
         out.println("Start");
-        String args2[] = {"f", "b", "r", "l", "f", "f", "r", "r", "f", "f", "f", "f", "f", "f", "f", "f"};
-        MoveDirection[] directions = new OptionsParser().parse(args2);
+        //String args2[] = {"f", "b", "r", "l", "f", "f", "r", "r", "f", "f", "f", "f", "f", "f", "f", "f"};
+        MoveDirection[] directions = new OptionsParser().parse(args);
         IWorldMap map = new RectangularMap(10, 5);
         Vector2d[] positions = { new Vector2d(2,2), new Vector2d(3,4) };
         IEngine engine = new SimulationEngine(directions, map, positions);
